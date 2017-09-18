2eff31a485d3e5520c3d916df76b6461bfe21532
Gary Scopel <Gary@Garys-MacBook-Air.local>
2017-09-18 16:36:10 -0400

    send email post request

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,54 @@ var express = require('express');
 var router = express.Router();
 var helper = require('sendgrid').mail;
 
+router.post('/:action', function(req, res, next){
+
+  var action = req.params.action
+
+  if (action == 'send'){// send the email
+
+  var from_email = new helper.Email('garyscopel1991@gmail.com');
+  var to_email = new helper.Email(req.body.recipient);
+  var subject = req.body.subject
+  var content = new helper.Content('text/html', req.body.content);
+  var mail = new helper.Mail(from_email, subject, to_email, content);
+
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+  });
+
+  sg.API(request, function(error, response) {
+    if (error){
+      res.json({
+        confirmation: 'fail',
+        message: error
+      })
+      return
+    }
+    res.json({
+      confirmation: 'success',
+      response: response
+    })
+    // console.log(response.statusCode);
+    // console.log(response.body);
+    // console.log(response.headers);
+  });
+
+    // res.json({
+    //   confirmation: 'success',
+    //   action: action
+    // })
+    return
+  }
+    res.json({
+      confirmation: 'fail',
+      message: 'Invalid Action'
+    })
+})
+
 
 /* GET users listing. */
 router.get('/:action', function(req, res, next) {
